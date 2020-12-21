0828a42df841c1816e24953da005803e2b8fae22
JoseSachango <62193290+JoseSachango@users.noreply.github.com>
2020-12-21 13:33:30 -0600

    Trying to get the username column to appear

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.use(
     
     });
 
-db.sequelize.sync().then(()=>{
+db.sequelize.sync({force:true}).then(()=>{
 
     server.listen(PORT,function(){
         console.log("Server up and running. Listening on  http://localhost:"+PORT)
